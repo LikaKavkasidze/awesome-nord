0cf8931596243c1a721e8b7434b4112403771e78
TitiAlone <talone@boxph.one>
2020-09-16 12:47:26 +0200

    Switch from ALSA to PulsaAudio

--- helpers.lua
+++ helpers.lua
@@ -26,40 +26,21 @@ helpers.keyboard_layout = function(step)
     awful.spawn.with_shell("setxkbmap "..layouts[current_layout])
 end
 
--- Volume control using ALSA
+-- Volume control using PulseAudio
 -- Pass a value between -100 and +100
 -- Positive values increase the volume, negative values decreases it.
 -- If zero is given, then mute is toggled
 helpers.volume_control = function(step)
     local cmd
 
-    -- Match if we're using headphones
-    awful.spawn.with_line_callback([[bash -c 'cat "/proc/asound/card1/codec#0"  \
-        | grep -A4 "Headphone Playback Switch"  \
-        | grep "Amp-Out vals:  \[0x00 0x00\]"   \
-        > /dev/null; echo $?']], {
-        stdout = function(line)
-            local device
+    if step == 0 then
+        cmd = "pactl set-sink-mute 0 toggle"
+    else
+        pactl_value = step > 0 and "+"..tostring(step).."%" or "-"..tostring(-step).."%"
+        cmd = "pactl set-sink-volume 0 "..pactl_value
+    end
 
-            if line == "0" then
-                device = "Headphone"
-            else
-                device = "Speaker"
-            end
-
-            if step == 0 then
-                cmd = "amixer -q sset "..device.." toggle_muted"
-            else
-                amixer_value = step > 0 and tostring(step).."%+" or tostring(-step).."%-"
-                cmd = "amixer -q sset "..device.." unmuted && amixer -q sset "..device.." "..amixer_value
-            end
-        
-            awful.spawn.with_shell(cmd)
-        end,
-        stderr = function(line)
-            naughty.notify { text = "ERR: "..line }
-        end,
-    })
+    awful.spawn.with_shell(cmd)
 end
 
 luminosity_storage = ""
